c182ffc30df3ac93645be756a2357146996ca988
Vishvam2006 <vishvam.r.modi@gmail.com>
2026-04-01 10:36:33 +0530

    brevo

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -1,30 +1,29 @@
-import nodemailer from "nodemailer";
+import SibApiV3Sdk from "sib-api-v3-sdk";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.BREVO_SMTP_MAIL,
-    pass: process.env.BREVO_SMTP_PASS,
-  },
-  debug: true,
-  logger: true,
-});
+const client = SibApiV3Sdk.ApiClient.instance;
+const apiKey = client.authentications["api-key"];
+apiKey.apiKey = process.env.BREVO_API_KEY;
 
 export const sendOTP = async (email, otp) => {
   try {
-    const info = await transporter.sendMail({
-      from: `"DayFlow" <${process.env.SENDER_EMAIL}>`,
-      to: email,
-      subject: "Login OTP",
-      text: `Your OTP is ${otp}`,
-    });
+    const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 
-    console.log("Email sent:", info.response);
+    const sendSmtpEmail = {
+      sender: {
+        email: process.env.SENDER_EMAIL,
+        name: "DayFlow",
+      },
+      to: [{ email }],
+      subject: "Login OTP",
+      textContent: `Your OTP is ${otp}`,
+    };
+
+    const data = await apiInstance.sendTransacEmail(sendSmtpEmail);
+
+    console.log("Email sent:", data);
   } catch (error) {
     console.error("MAIL ERROR:", error);
   }
